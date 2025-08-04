7d5b86a422507e301d30dd57fee77989f9f0e079
yinyue2 <yinyue2@sugon.com>
2025-08-04 16:00:08 +0800

    fix: UI

---
+++ src/Vicold.Atmospex.Render/Vicold.Atmospex.Render.Frame/EverLayer.cs
@@ -0,0 +1,14 @@
+﻿using Evergine.Common.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vicold.Atmospex.Render.Frame
+{
+    public class EverLayer
+    {
+        private RenderLayerDescription _customLayer;
+    }
+}
